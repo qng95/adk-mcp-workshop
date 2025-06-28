5b1ae47868e3c2db6a277e2ee8c9a2f7c1f4e6f7
Quan Nguyen <quan.nguyen.itconsulting@gmail.com>
2025-06-28 13:52:52 +0200

    use the proper one

--- my_noob_dj/agent.py
+++ my_noob_dj/agent.py
@@ -13,9 +13,9 @@ load_dotenv() # load environment variables from .env file
 # These are all free to use models, but they have different limits on the number of requests per day.
 # In total we will 1600 requests per day to try
 
-GEMINI_MODEL = "gemini-2.0-flash-lite" # 1000 requests / day
+#GEMINI_MODEL = "gemini-2.0-flash-lite" # 1000 requests / day
 #GEMINI_MODEL = "gemini-2.5-flash" # 250 requests / day
-#GEMINI_MODEL = "gemini-2.0-flash" # 200 requests / day
+GEMINI_MODEL = "gemini-2.0-flash" # 200 requests / day
 #GEMINI_MODEL = "gemini-2.0-flash-exp" # 50 requests / day
 #GEMINI_MODEL = "gemini-1.5-flash" # 50 requests / day
 #GEMINI_MODEL = "gemini-1.5-flash-8b" # 50 requests / day
